6ebf503f14720889ecabd7afe84441cb88a079ee
Allan <j.a.o.huthan@gmail.com>
2022-10-24 12:54:25 -0300

    mobile version

---
+++ scripts.js
@@ -0,0 +1,28 @@
+const form = document.querySelector("form");
+const email = document.getElementById("email");
+const emailError = document.querySelector(".error");
+const emailErrorIcon = document.getElementById("error-icon");
+
+email.addEventListener("change", (event) => {
+  if (!email.validity.valid) {
+    emailErrorIcon.style.display = "block";
+    showError();
+    event.preventDefault();
+  } else {
+    emailErrorIcon.style.display = "none";
+    emailError.innerText = "";    
+  }
+});
+
+form.addEventListener("submit", (event) => {
+  if (!email.validity.valid) {
+    showError();
+    event.preventDefault();
+  }
+});
+
+function showError() {
+  if (email.validity.typeMismatch) {
+    emailError.innerText = "Please provide a valid email";
+  }
+}
